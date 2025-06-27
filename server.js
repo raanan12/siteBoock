9fb5fcbd3c4158e7af0878363eaceb5391c1f0bc
רענן אוריאל <rnnwryl@MacBook-Air-sl-rnn.local>
2025-06-27 03:42:30 +0300

    Initial commit

--- server.js
+++ server.js
@@ -729,8 +729,11 @@ constantFunction()
 // test2()
 // createManger()
 
-app.listen(3000, () => console.log('server port on 3000'))
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+});
 
 
 
